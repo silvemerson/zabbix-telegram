a0f9f93d446b3723efe569da721388f7352222e3
Emerson Silva <emerson.silva@4linux.com.br>
2022-03-11 16:23:33 -0300

    Atualizado para Python3 e adicionado o modulo os.environ

--- telegram.py
+++ telegram.py
@@ -2,12 +2,15 @@
 
 import sys
 import telebot
+import os
 
-BOT_TOKEN= "1356580905:AAGfBD5oSjUDgQoYJsxs1if88k_2OVymWeI"
-DESTINATION=sys.argv[1]
-SUBJECT=sys.argv[2]
-MESSAGE=sys.argv[3]
+BOT_TOKEN = os.environ['BOT_TOKEN']
+DESTINATION = sys.argv[1]
+SUBJECT = sys.argv[2]
+MESSAGE= sys.argv[3]
 
 MESSAGE = MESSAGE.replace('/n','\n')
 tb = telebot.TeleBot(BOT_TOKEN)
 tb.send_message(DESTINATION,SUBJECT + '\n' + MESSAGE)
+
+
